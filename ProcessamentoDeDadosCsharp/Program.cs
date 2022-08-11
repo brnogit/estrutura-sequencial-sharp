43c18395dabe67dcd0c11b65f35332f931a328a5
bmotadev <bmotadev@gmail.com>
2022-08-11 12:22:05 -0300

    exemplo 2 - processamento de dados

---
+++ ProcessamentoDeDadosCsharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProcessamentoDeDadosCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x, y;
+            //x = 5;
+            //y = 2 * x;
+
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+            //Console.ReadLine();
+            //---------------------------------------------------------------------------
+            int x;
+            double y;
+            x = 5;
+            y = 2 * x;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            Console.ReadLine();        
+        }
+    }
+}

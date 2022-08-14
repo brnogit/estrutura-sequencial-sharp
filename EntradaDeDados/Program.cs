7b7f8775fa529eb50b96d41d24943eb3d0387c71
bmotadev <bmotadev@gmail.com>
2022-08-13 23:02:52 -0300

    Entrada de Dados Parte1

---
+++ EntradaDeDados/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EntradaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = Console.ReadLine();
+
+            Console.Write("Você digitou: " + frase);
+        }
+    }
+}

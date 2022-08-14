8dd4b12e26a010924cc3812fbc67ca7617b65eab
bmotadev <bmotadev@gmail.com>
2022-08-13 23:15:14 -0300

    Utilizando o split

--- EntradaDeDados/Program.cs
+++ EntradaDeDados/Program.cs
@@ -7,8 +7,24 @@ namespace EntradaDeDados
         static void Main(string[] args)
         {
             string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
 
-            Console.Write("Você digitou: " + frase);
-        }
+            //string s = Console.ReadLine();
+
+            string[] v = s.Split(' ');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
+            Console.Write("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);        }
     }
 }
